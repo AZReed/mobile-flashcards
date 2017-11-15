486f31e20763c3de1e9c380b7559c633cad00397
AZReed <azreed@gmail.com>
2017-11-15 17:58:45 -0300

    feat: react-native-elements installed

--- components/Deck.js
+++ components/Deck.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text } from "react-native";
+import { Button } from "react-native-elements";
 
 export default class Deck extends Component {
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.item}>
-          <Text>
-            hello
-          </Text>
+          <Text>hello</Text>
         </View>
         <View style={styles.item}>
-          <Text>
-            hello
-          </Text>
+          <Button
+            raised
+            icon={{ name: "home", size: 32 }}
+            buttonStyle={{ backgroundColor: "red", borderRadius: 10 }}
+            textStyle={{ textAlign: "center" }}
+            title={`Welcome to\nReact Native Elements`}
+          />
         </View>
       </View>
     );
@@ -23,9 +26,9 @@ export default class Deck extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center"
   },
   item: {
-    alignItems: 'center',
+    alignItems: "center"
   }
 });
