cd3b48381d4b1769dafd5e1f8f066da3e23ea4a9
AZReed <azreed@gmail.com>
2017-12-04 14:02:59 -0300

    #1 review: stateless component

--- components/CardQuiz.js
+++ components/CardQuiz.js
@@ -1,61 +1,71 @@
-import React, { Component } from "react";
+import React from "react";
 import { StyleSheet } from "react-native";
-import { H1, H3, Card, CardItem, Body, Content, Container, Text, Button } from "native-base";
+import {
+  H1,
+  H3,
+  Card,
+  CardItem,
+  Body,
+  Content,
+  Container,
+  Text,
+  Button
+} from "native-base";
 
-class CardQuiz extends Component {
+const CardQuiz = (props) => {
+  const card = value => (
+    <Card>
+      <CardItem style={styles.cardPosition}>
+        <Body>
+          <H3>{props.remaining}</H3>
+          <H1>
+            {value.charAt(0).toUpperCase() + value.slice(1)}:{" "}
+            {props.data[value]}
+          </H1>
+        </Body>
+      </CardItem>
+    </Card>
+  );
 
-	card = (value) =>
-		<Card>
-			<CardItem style={styles.cardPosition}>
-				<Body>
-					<H3>
-						{this.props.remaining}
-					</H3>
-					<H1>
-						{value.charAt(0).toUpperCase() + value.slice(1)}: {this.props.data[value]}
-					</H1>
-				</Body>
-			</CardItem>
-		</Card>
-
-  render() {
-    return (
-			<Container>
-				{this.props.showAnswerState === false ?
-						(
-						<Content>
-							{this.card('question')}
-							<Container style={{flex: 1, alignSelf: 'center'}}>
-								<Button onPress={() => this.props.showAnswer()}>
-									<Text>Show Answer</Text>
-								</Button>
-							</Container>
-						</Content>
-						)
-						: (
-						<Content>
-							{this.card('answer')}
-							<Container style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around'}}>
-								<Button success onPress={() => this.props.guess(true)}>
-									<Text>Correct</Text>
-								</Button>
-								<Button danger onPress={() => this.props.guess(false)}>
-									<Text>Incorrect</Text>
-								</Button>
-							</Container>
-						</Content>
-						)
-				}
-			</Container>
-		);
-  }
+	return (
+		<Container>
+			{props.showAnswerState === false ? (
+				<Content>
+					{card("question")}
+					<Container style={{ flex: 1, alignSelf: "center" }}>
+						<Button onPress={() => props.showAnswer()}>
+							<Text>Show Answer</Text>
+						</Button>
+					</Container>
+				</Content>
+			) : (
+				<Content>
+					{card("answer")}
+					<Container
+						style={{
+							flex: 1,
+							flexDirection: "row",
+							justifyContent: "space-around"
+						}}
+					>
+						<Button success onPress={() => props.guess(true)}>
+							<Text>Correct</Text>
+						</Button>
+						<Button danger onPress={() => props.guess(false)}>
+							<Text>Incorrect</Text>
+						</Button>
+					</Container>
+				</Content>
+			)}
+		</Container>
+	);
 }
 
 export default CardQuiz;
 
 const styles = StyleSheet.create({
   cardPosition: {
-		flex: 1,
-		alignItems: 'center'
-	}
+    flex: 1,
+    alignItems: "center"
+  }
 });
